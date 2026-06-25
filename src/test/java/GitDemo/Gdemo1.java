c851d9f5e632fe9ba1f82393ed0f3980def25475
HP <HP@NRJHP-24>
2026-06-25 14:25:50 +0530

    Fist time commiting.

---
+++ src/test/java/GitDemo/Gdemo1.java
@@ -0,0 +1,11 @@
+package GitDemo;
+
+import org.testng.annotations.Test;
+
+public class Gdemo1 {
+	@Test(groups="smoke")
+	public void movieTest() {
+		System.out.println("Movie Name: Finding The Nemo");
+	}
+
+}
---
+++ src/test/java/GitDemo/Gdemo2.java
@@ -0,0 +1,13 @@
+package GitDemo;
+
+import org.testng.annotations.Test;
+
+public class Gdemo2 {
+
+		@Test
+		public void movieTest2() {
+			System.out.println("Movie Name: Django");
+		}
+
+	}
+
